d805cfa6c201f8cd7b61faf6063d841e12d2f9a1
Patrick Stenger <p.stenger@esatus.com>
2022-09-08 11:50:51 +0200

    added todo comment

--- src/Hyperledger.Aries/Features/Handshakes/DidExchange/DidRecord.cs
+++ src/Hyperledger.Aries/Features/Handshakes/DidExchange/DidRecord.cs
@@ -7,6 +7,9 @@ namespace Hyperledger.Aries.Features.Handshakes.DidExchange
 {
     public class DidRecord : RecordBase
     {
+
+        //TODO : ??? - Maybe add more info like didmethod, verkey_type, metadata, (see acapy implementation -> create_local_did and more)
+        // Also add Tags where dids are created? -> fetch_all via tagfilter possible
         public override string TypeName => "AF.DidRecord";
 
         public string Did { get; set; }
